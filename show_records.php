7176226df62cc779b7757f2802ef8c092ff15169
satansgrillmaster <maetthe_hollenstein@hotmail.com>
2022-01-19 16:17:26 +0100

    dasd

--- show_records.php
+++ show_records.php
@@ -1,25 +1,44 @@
 <?php
 
+#setup db
+require_once('./include/connect.php');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+//if delete is clicked, all selected records will be removed
+if(isset($_POST['delete'])){
+    $checkboxes = $_POST['record_check'];
+    $delete_string = '';
+    foreach ($checkboxes as $id){
+        if($id != ''){
+            $delete_string = $delete_string . $id . ",";
+        }
+    }
+
+    $sql = "delete from buch where buch_id in ($delete_string 0)";
+    $db->exec($sql);
+}
+
+// select all records to show in table
+$sql = 'select * from buch';
+$result = $db->query($sql);
+
+$db = null;
+
 header('Content-Type: text/html; charset=UTF-8');
 include('navigation.html');
 echo '<div class="container">';
 
-#print all records from table buch
-echo '<table>';
-require_once('./include/connect.php');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$sql = 'select * from buch';
-$result = $db->query($sql);
-$db = null;
-
 echo '<h1>Bücher</h1>';
+echo '<form method="post" action="show_records.php" accept-charset="utf-8">';
 echo '<table>';
 foreach ($result as $value){
-    echo '<tr>';
+    echo '<tr><td><input type="checkbox" name="record_check[]" value="'. $value['buch_id'].'"></td>';
+
     foreach ($value as $column){
         echo '<td>'.$column.'</td>';
     }
     echo '</tr>';
 }
 echo '</table>';
-echo '</div></body></html>';
+echo '<input type="submit" name="delete" value="Datensätze löschen">';
+echo '</form></div></body></html>';
